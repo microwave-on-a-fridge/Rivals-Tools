e20d157c1869452a2e0970d59f745a8a45e67e97
microwave on a fridge <pastel-x64@proton.me>
2022-12-14 19:00:38 +1300

    oopsy has been fixed

--- replays.bat
+++ replays.bat
@@ -3,7 +3,6 @@
 set driveLetter=
 set /p driveLetter=Welcome to the Rivals of Aether replay extraction tool! What drive letter would you like the replays to be copied to?
 echo \n
-if not '%unverum%'=='' set unverum=%unverum:~0,1%
 xcopy /s /q "%LOCALAPPDATA%\RivalsofAether\replays" "%driveLetter%:\Rivals of Aether Replays\"
 echo Extraction complete.
 @pause
