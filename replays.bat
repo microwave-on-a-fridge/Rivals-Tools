b2f3b40aa36dd05bdb8c3c6d7a207990db5dfe91
microwave on a fridge <108324217+microwave-on-a-fridge@users.noreply.github.com>
2022-12-17 21:24:44 +1300

    gooey
    added simple gui to the programs

--- replays.bat
+++ replays.bat
@@ -1,9 +1,6 @@
-@echo off
-:start
-set driveLetter=
-set /p driveLetter=Welcome to the Rivals of Aether replay extraction tool! What drive letter would you like the replays to be copied to?
-echo \n
+@echo off 
+powershell -Command "& {Add-Type -AssemblyName Microsoft.VisualBasic; [Microsoft.VisualBasic.Interaction]::InputBox('Welcome to the Rivals of Aether Replay Extraction Tool! What drive letter would you like your replays to be copied to?', 'Rivals of Aether Replay Extraction Tool')}" > %TEMP%\out.tmp
+set /p driveLetter=<%TEMP%\out.tmp
 xcopy /s /q "%LOCALAPPDATA%\RivalsofAether\replays" "%driveLetter%:\Rivals of Aether Replays\"
-echo Extraction complete.
-@pause
-exit
+set msgBoxArgs="& {Add-Type -AssemblyName System.Windows.Forms; [System.Windows.Forms.MessageBox]::Show('Extraction successful. Replays have been copied to %driveLetter%:\Rivals of Aether Replays\', 'Extraction Successful', 'OK', [System.Windows.Forms.MessageBoxIcon]::Information);}"
+powershell -Command %msgBoxArgs%
--- saves.bat
+++ saves.bat
@@ -1,9 +1,6 @@
-@echo off
-:start
-set driveLetter=
-set /p driveLetter=Welcome to the Rivals of Aether save data extraction tool! What drive letter would you like your save data to be copied to?
-echo \n
+@echo off 
+powershell -Command "& {Add-Type -AssemblyName Microsoft.VisualBasic; [Microsoft.VisualBasic.Interaction]::InputBox('Welcome to the Rivals of Aether Save Data Extraction Tool! What drive letter would you like your saves to be copied to?', 'Rivals of Aether Save Data Extraction Tool')}" > %TEMP%\out.tmp
+set /p driveLetter=<%TEMP%\out.tmp
 xcopy /s /q "%LOCALAPPDATA%\RivalsofAether\saves" "%driveLetter%:\Rivals of Aether Save Data\"
-echo Extraction complete.
-@pause
-exit
+set msgBoxArgs="& {Add-Type -AssemblyName System.Windows.Forms; [System.Windows.Forms.MessageBox]::Show('Extraction successful. Saves have been copied to %driveLetter%:\Rivals of Aether Save Data\', 'Extraction Successful', 'OK', [System.Windows.Forms.MessageBoxIcon]::Information);}"
+powershell -Command %msgBoxArgs%
